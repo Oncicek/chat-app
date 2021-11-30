98df28e5857204db699a4f5317220f78c7eb0275
Tomáš Králík <tkralik@memos.cz>
2021-11-30 23:46:57 +0100

    new db

--- README.md
+++ README.md
@@ -1,35 +1,40 @@
 # chat-app
 
-# Firebase test database expired. Should you want to try my app out, please contact me on kralik.tomase@gmail.com and I´ll setup a new one.
-
 ## Project setup
+
 ```
 npm install
 ```
 
 ### Compiles and hot-reloads for development
+
 ```
 npm run serve
 ```
 
 ### Compiles in Electron as a desktop app
+
 ```
 npm run electron:serve
 ```
 
 ### Compiles and minifies for production
+
 ```
 npm run build
 ```
 
 ### Lints and fixes files
+
 ```
 npm run lint
 ```
 
 ## LOGIN:
+
 Email: lacika@kutyil.sk
 Pwd: andrejka
 
 ### Customize configuration
+
 See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/db.ts
+++ src/db.ts
@@ -1,16 +1,16 @@
-/* eslint-disable */
 import firebase from 'firebase/compat/app'
 import 'firebase/compat/database'
 import 'firebase/compat/auth'
 
 const config = {
-    apiKey: "AIzaSyBUvUZi0rUMoYyb7oQsJaLesVKswvAHYMk",
-    authDomain: "mustafar-chat-app.firebaseapp.com",
-    projectId: "mustafar-chat-app",
-    storageBucket: "mustafar-chat-app.appspot.com",
-    messagingSenderId: "616029607397",
-    appId: "1:616029607397:web:055f1f9aa01fad10914b0c",
-    measurementId: "G-T53P7LNYS4"
+    apiKey: "AIzaSyCY3oraSCuMe53GYe0suW7QpesY2BDPCOw",
+    authDomain: "chat-app-97db9.firebaseapp.com",
+    databaseURL: "https://chat-app-97db9-default-rtdb.firebaseio.com",
+    projectId: "chat-app-97db9",
+    storageBucket: "chat-app-97db9.appspot.com",
+    messagingSenderId: "49816693073",
+    appId: "1:49816693073:web:f9c4b1c1e206e11fb84b70",
+    measurementId: "G-E03B46EPT6"
 }
 
 const db = firebase.initializeApp(config)
